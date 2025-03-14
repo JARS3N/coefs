5624652041a261cd197d78d6389dbe624f281041
JARS3N <JARS3N@users.noreply.github.com>
2025-03-14 16:36:46 -0400

    Update server.R

--- R/server.R
+++ R/server.R
@@ -1,5 +1,3 @@
-#' Shiny server logic for the app
-#' @export
 server <- function(input, output, session) {
   library(shiny)
   library(DBI)
@@ -35,8 +33,12 @@ server <- function(input, output, session) {
     filtered_lots <- lotstuff
 
     if (input$Type != "All") {
-      filtered_lots <- filtered_lots[filtered_lots$LotNumber %in% grep(paste0("^", input$Type), filtered_lots$LotNumber, value = TRUE), ]
+      filtered_lots <- filtered_lots[grep(paste0("^", input$Type), filtered_lots$LotNumber), ]
     }
+
+    # Debugging: Print filtered lots
+    cat("Filtered Lots:\n")
+    print(filtered_lots)
 
     updateSelectInput(session, 'Lot', choices = filtered_lots$LotNumber)
   })
@@ -46,8 +48,39 @@ server <- function(input, output, session) {
     if (!is.null(input$Lot) && input$Lot != "SELECT") {
       BMID <- lotstuff$BMID[lotstuff$LotNumber == input$Lot]
 
+      # Debugging: Print BMID
+      cat("Selected BMID:", BMID, "\n")
+
       info <- coefs::fetch(BMID)
+
+      # Debugging: Print fetched data
+      cat("Fetched Data for BMID:", BMID, "\n")
+      print(info)
+
       output$Lottable <- renderTable(data.frame(Lot = input$Lot))
+
+      output$pHtable <- renderTable({
+        if (is.null(info) || nrow(info) == 0) {
+          return(data.frame(Message = "No data available"))
+        }
+        dplyr::select(info, dplyr::contains("PH")) %>%
+          dplyr::mutate(PH_A = as.character(round(PH_A, 0)))
+      }, digits = 6)
+
+      output$oxtable <- renderTable({
+        if (is.null(info) || nrow(info) == 0) {
+          return(data.frame(Message = "No data available"))
+        }
+        dplyr::select(info, dplyr::contains("O2")) %>%
+          dplyr::mutate(O2_A = as.character(round(O2_A, 0)))
+      }, digits = 6)
+
+      output$bftbl <- renderTable({
+        if (is.null(info) || nrow(info) == 0 || is.na(info$BF)) {
+          return(data.frame(Cartridge_BufferFactor = "No Data"))
+        }
+        dplyr::select(info, Cartridge_BufferFactor = BF)
+      }, digits = 6)
 
       # If checkbox is checked, find all lots with the same BMID
       if (input$show_matching_lots) {
